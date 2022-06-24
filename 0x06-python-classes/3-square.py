f5d93157db5eec4ac16a354eca43ceffc4b510eb
Murugi <lizar.murugi@gmail.com>
2022-06-24 00:58:35 -0700

    defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
+"""
+Define a Square class
+"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    Public instance method: def area(self).
+    """
+    A class that defines a square
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
+        """
+        Initializes the square
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """Returns the current square area."""
+        """
+        Returns the current square area
+        """
         return self.__size ** 2
