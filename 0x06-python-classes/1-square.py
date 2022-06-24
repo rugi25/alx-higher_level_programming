615b63041a2a2a1a2a51aec3afcb380caa0320c7
Murugi <lizar.murugi@gmail.com>
2022-06-24 00:52:47 -0700

    defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
+    """ class Square that defines a square"""
 
     def __init__(self, size):
-        """Initializes the data."""
-        self.__size = size
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
