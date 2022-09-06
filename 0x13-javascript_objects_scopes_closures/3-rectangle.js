e4523805a8b98a079c9db08a5cfa5b6dfa5f676f
Murugi <lizar.murugi@gmail.com>
2022-09-06 03:10:10 -0700

    Correction of task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-    for (let iter = 0; iter < this.height; iter++) {
-      console.log('X'.repeat(this.width));
-    }
+    console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
   }
-};
+}
+module.exports = Rectangle;
